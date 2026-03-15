03b98f9a9af1c6766464e1444e54bf3e0170b8ce
EltonSeferli <eltonseferli2@gmail.com>
2026-03-15 16:05:07 +0000

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
+#!usr/bin/python3
 def no_c(my_string):
-    """Return a string without characters 'c' and 'C'"""
     new_string = ""
-
     for ch in my_string:
         if ch != 'c' and ch != 'C':
             new_string += ch
-
     return new_string
