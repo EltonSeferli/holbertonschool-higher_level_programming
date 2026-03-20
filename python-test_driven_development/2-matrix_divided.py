599a30b4f2740a466d505ed7b1566290481dd0e0
EltonSeferli <eltonseferli2@gmail.com>
2026-03-20 18:26:52 +0000

    add usr/bin

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,5 @@
+#!/usr/bin/python3
 def matrix_divided(matrix,div):
-
-#     matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6]
-# ]
-
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
