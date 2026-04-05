2a3413feafeb1792b371f9c4030b4f4e7545668c
ELTON <162188565+EltonSeferli@users.noreply.github.com>
2026-04-05 20:30:49 +0400

    Add files via upload

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Give all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create table if it doesn't exist
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
